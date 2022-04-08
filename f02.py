d98889575c2cc7345a3b2052288b3bacfb60bc50
Arleen <89202471+arleenchr@users.noreply.github.com>
2022-04-08 20:57:24 +0700

    Update f02.py

---
+++ f02.py
@@ -0,0 +1,11 @@
+'''
+MODUL F02-REGISTER
+Spesifikasi: Admin dapat mendaftarkan pengguna baru
+I.S. : input nama, username, password
+F.S. : pengguna terdaftar sebagai user
+'''
+
+# KAMUS LOKAL
+# 
+
+#def register (x):
