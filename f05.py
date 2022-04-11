5151035ec1a90ea438d20a02a20d3e51ef1d7d5c
Arleen <89202471+arleenchr@users.noreply.github.com>
2022-04-11 22:26:17 +0700

    Update f05.py
    Perlu direvisi: nama folder tempat game.csv disimpan, penggunaan fungsi save

--- f05.py
+++ f05.py
@@ -3,7 +3,6 @@ MODUL F05-Mengubah Game pada Toko Game
 Spesifikasi: Mengubah game pada toko game dengan memasukkan data
 I.S. : input id, nama, kategori, tahun rilis, dan harga game
 F.S. : game.csv diperbaharui
-
 Desainer dan coder : 16521172, 16521199
 '''
 
@@ -14,6 +13,7 @@ Desainer dan coder : 16521172, 16521199
 # ALGORITMA
 # IMPORT MODUL
 import f16 as save_changes
+from f_csvparser import csv_to_arr
 
 # REALISASI FUNGSI
 #Fungsi untuk mengubah game pada toko oleh admin
@@ -24,13 +24,19 @@ def ubah_game():
     tahun_rilis = input("Masukkan tahun rilis: ")
     harga = input("Masukkan harga: ")
     
-    # akses file game.csv
-    #folder = 'admin'
-    #path = folder + '\game.csv'
-    #file = open(path,'r')
-    #lines = file.readlines()
+    # deklarasikan array file game.csv
+    file_game = csv_to_arr('game','csv_files')
+    arr_id = [file_game[i][0] for i in range (len(file_game))]    
     
     # cari id game
-    
-        
-    save_changes
+    for i in range (len(file_game)):
+        if id_game == arr_id[i]:
+            if nama_game != '':
+                file_game[i][1] = nama_game
+            if kategori != '':
+                file_game[i][2] = kategori
+            if tahun_rilis != '':
+                file_game[i][3] = tahun_rilis
+            if harga != '':
+                file_game[i][4] = harga
+            save_changes()
