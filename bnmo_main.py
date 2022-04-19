3bf316773d9da0c424db4c4782dc4459c837f023
Arleen <16521172@mahasiswa.itb.ac.id>
2022-04-19 22:49:31 +0700

    update f15, bnmo_main
    bnmo_main : masih error jika nama_folder tidak diinputkan. belum bisa memanggil argumen/modul lain setelah berhasil load

--- bnmo_main.py
+++ bnmo_main.py
@@ -1,10 +1,13 @@
 #main
+import argparse
+import os
+import sys
 import f_csvparser as csvparser
 import f_common as common
-# import f02 as f02
-# import f03 as f03
+import f02 as f02
+import f03 as f03
 import f04 as f04
-# import f05 as f05
+import f05 as f05
 import f06 as f06
 import f07 as f07
 import f08 as f08
@@ -12,28 +15,52 @@ import f09 as f09
 import f10 as f10
 import f11 as f11
 import f12 as f12
-# import f13 as f13
-# import f14 as f14
-# import f15 as f15
-# import f16 as f16
-# import f17 as f17
+import f13 as f13
+import f14 as f14
+import f15 as f15
+import f16 as f16
+import f17 as f17
 
 # KAMUS GLOBAL
 # variabel array dari csv : game; kepemilikan; riwayat; user
 
-# f07 prosedur
+
+
+# LOAD
+parser = argparse.ArgumentParser()
+parser.add_argument('folder',type=str)
+
+args = parser.parse_args()
+
+path = '.\\saves\\' + args.folder # path folder
+isFolderExist = os.path.exists(path) # cek apakah folder ada
+
+if args.folder=='':
+    print("Tidak ada nama folder yang diberikan!")
+    print('Usage: python bnmo_main.py <nama_folder>')
+else:
+    if not isFolderExist:
+        print('Folder', '"%s"' % args.folder, 'tidak ditemukan.')
+    else:
+        f15.load(args.folder)
+        
+        #parser = argparse.ArgumentParser(prog="%s %s" % (os.path.basename(path.argv[0]), function))
+        
+        
+        
+
 
 
 # tes
-kepemilikan = csvparser.csv_to_arr('kepemilikan', 'save0')
-game = csvparser.csv_to_arr('game', 'save0')
-riwayat = csvparser.csv_to_arr('riwayat', 'save0')
-user = csvparser.csv_to_arr('user', 'save0')
+#kepemilikan = csvparser.csv_to_arr('kepemilikan', 'save0')
+#game = csvparser.csv_to_arr('game', 'save0')
+#riwayat = csvparser.csv_to_arr('riwayat', 'save0')
+#user = csvparser.csv_to_arr('user', 'save0')
 
 
-userinventory = common.create_inventory_arr(kepemilikan, game, 0) # admin id 0
-tokogame = common.create_tokogame_arr(game)
-userhistory = common.create_userhistory_arr(riwayat, 0)
+#userinventory = common.create_inventory_arr(kepemilikan, game, 0) # admin id 0
+#tokogame = common.create_tokogame_arr(game)
+#userhistory = common.create_userhistory_arr(riwayat, 0)
 #sebelum
 #print('sebelum')
 #for i in range(1, common.iterLength(user)):
--- f15.py
+++ f15.py
@@ -8,37 +8,22 @@ Desainer dan coder : 16521172, 16521199
 
 # KAMUS LOKAL
     # FUNGSI DAN PROSEDUR
-        #
+        # csv_to_arr (input filename, folder : string, output arr : array of array of string)
     # VARIABEL
-        # path : string
-        # isFolderExist : boolean
-        # user, game, riwayat, inventory : array of array of string
+        # user, game, riwayat, kepemilikan : array of array of string
     
 # ALGORITMA
 # IMPORT MODUL DAN FUNGSI
-import f_common as common
 from f_csvparser import csv_to_arr
-import os
-import argparse
 
+# REALISASI FUNGSI
 def load (nama_folder):
-    # cek apakah nama_folder ada atau tidak
-    path = '.\\saves\\' + nama_folder # path folder
-    isFolderExist = os.path.exists(path) # cek apakah folder ada
-    
-    # output
-    if nama_folder==None: # jika tidak ada parameter
-        print("Tidak ada nama folder yang diberikan!")
-        print("Usage: python program_binomo.py <nama_folder>")
-    else:
-        if not isFolderExist:
-            print("Folder",'"%s"' % nama_folder,"tidak ditemukan.")
-        else:
-            print("Loading...")
-            user = csv_to_arr('user',nama_folder)
-            game = csv_to_arr('game',nama_folder)
-            riwayat = csv_to_arr('riwayat',nama_folder)
-            inventory = csv_to_arr('kepemilikan',nama_folder)
-            print("Selamat datang di antarmuka", '"%s"' % "Binomo")
-
-#load ('save0')
+    print("Loading...")   # mencetak ke layar
+    # menuliskan data pada file csv ke dalam bentuk array of array of string
+    user = csv_to_arr('user',nama_folder)
+    game = csv_to_arr('game',nama_folder)
+    riwayat = csv_to_arr('riwayat',nama_folder)
+    kepemilikan = csv_to_arr('kepemilikan',nama_folder)
+    # output (cetak ke layar)
+    print("Selamat datang di antarmuka", '"%s"' % "Binomo")
+    return user,game,riwayat,kepemilikan   # mengembalikan array
